2d99bca8e0e06127a15532d32db03273da38178f
Sean Starnes <seanstar12@gmail.com>
2013-09-05 23:47:19 -0500

    Cleaned up symlink check. Pass cleaned parameters to snatching functions.

--- app.js
+++ app.js
@@ -11,22 +11,25 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.cookieParser());
 app.use(express.static('public/'));
+app.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: 2592000000 }));
 app.listen(8089);
 
-filePath = '/storage/ School/';
+filePath = '/storage/ School';
 route = '/';
-allowHidden = true;
-allowSym = false; //not safe. by adding a slash, it bypasses.
+allowHidden = false;    //work in progress
+allowSym = false;
+
+var verify = [buildPath,symmCheck];
 
 app.get('*',verify, function (req,res) {
 
-  fs.stat(filePath + req.params , function(err, stats){
+  fs.stat(filePath + req._PATHSTR , function(err, stats){
     if (err) {
       if (err.code == 'ENOENT') res.send(['Doesn\'t Exist']);
       else throw err;
     }
     else if (stats.isDirectory()) {
-      fs.readdir(filePath + req.params, function(err, files){
+      fs.readdir(filePath + req._PATHSTR, function(err, files){
         var _list = [];
         files = files.sort();
 
@@ -34,44 +37,64 @@ app.get('*',verify, function (req,res) {
           var _url = '';
 
           if (hidden(/^\./,files[i])) {
-            _list.push({name:files[i],path:req.params+'/'+files[i]});
+            _list.push({name:files[i],path:req._PATHSTR+'/'+files[i]});
           }
         }
         res.send(_list);
       });
     } else if (stats.isFile()){
-      magic.detectFile(filePath + req.params, function(err,result){
+      magic.detectFile(filePath + req._PATHSTR, function(err,result){
         if (err) throw err; 
         else {
           res.writeHead(200, {
             'Content-Type': result,
             'Content-Length': stats.size
           });
-          var readStream = fs.createReadStream(filePath + req.params);
+          var readStream = fs.createReadStream(filePath + req._PATHSTR);
           readStream.pipe(res);
-          console.log(Date.now() + '  ' + filePath + req.params);
+          console.log(Date.now() + '  ' + filePath + req._PATHSTR);
         }
       });
     }
   });
 });
 
-function verify(req,res,next){
-  if (!allowSym){
-    magic.detectFile(filePath + req.params, function(err, result){
-      if (result.indexOf('symlink') < 0) pass();
-      else res.send('This is not the path you\'re looking for.');
-    });
-  }
-  else pass();
+function buildPath(req,res,next){
+  var split = '', valPath = [], str = '';
+  split = req.params.toString().split('/');
 
-  function pass(){
-    if (allowHidden) next();
-    else if (/\/\./.test(req.params)) {
-      res.send('This is not the path you\'re looking for.');
+  for (var _i =0; _i < split.length; _i++) {
+    if (split[_i]) {
+      valPath.push(split[_i]);
+      str += '/' + split[_i];
     }
-    else next();
   }
+
+  req._PATH = valPath;
+  req._PATHSTR = str;
+  next();
+}
+
+function symmCheck(req,res,next){
+  // We check every directory everytime to ensure that its not going through a symlink.
+  if (!allowSym && req._PATH[0]){
+    var bool = true, str = '', cnt = 0;
+    for (var i=0; i < req._PATH.length; i++){
+      str += '/' + req._PATH[i];
+      magic.detectFile(filePath + str, function(err, result){
+        cnt++;
+        if (err) res.send(['Doesn\'t exist']);
+        else {
+          if (result.indexOf('symlink') >= 0 ) bool = false;
+          if (cnt ==  req._PATH.length) {
+            if (bool) next();
+            else res.send('This is not the path you\'re looking for.');
+          }
+        }
+      });
+    }
+  }
+  else next();
 }
 
 function hidden(exp,str){
